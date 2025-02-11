06c967ae0734aff0dfe361d830f0ed8fdd46b38e
T-WREKX <abdulhosaint@gmail.com>
2025-02-11 13:28:14 +0530

    18

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,10 @@
 function ListGroup()
 {
 let items =['one ','two','three','four','five','six','seven']
-items = [];
     return (<ul className="list-group">
             <h1>List Name</h1>
             {items.length === 0 && <p>No item found</p>}
-            {items.map(item => <li key={item}> {item} </li>)}
+            {items.map(item => <li className="l-i-g" key={item} onClick={()=>console.log('clickedhere')}> {item} </li>)}
             </ul>);
 }
 export default ListGroup;
