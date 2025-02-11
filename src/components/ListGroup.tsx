fc9cd3c2309f9dd270f711f97813b38ec519a7ca
T-WREKX <abdulhosaint@gmail.com>
2025-02-11 13:19:30 +0530

    16

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,7 +4,7 @@ let items =['one ','two','three','four','five','six','seven']
 items = ['ten'];
     return (<ul className="list-group">
             <h1>List Name</h1>
-            {items.length === 0 ? <p>No item found</p> : null}
+            {items.length === 0 && <p>No item found</p>}
             {items.map(item => <li key={item}> {item} </li>)}
             </ul>);
 }
