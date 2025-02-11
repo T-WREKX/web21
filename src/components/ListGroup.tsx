18843096f3c507acfed83d8ab7fbd63d15d720d2
T-WREKX <abdulhosaint@gmail.com>
2025-02-11 12:38:01 +0530

    12

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,7 +1,7 @@
 function ListGroup()
 {
 let items =['one ','two','three','four','five','six','seven']
-items = [];
+items = ['three'];
     return (<ul className="list-group">
             <h1>List Name</h1>
             {items.length === 0 ? <p>No item found</p> : null}
