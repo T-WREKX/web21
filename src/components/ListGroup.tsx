4ad7a0d04e4e81d8d785ab32a3c39b9bf1ece3c9
T-WREKX <abdulhosaint@gmail.com>
2025-02-11 12:31:18 +0530

    9

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,7 +3,7 @@ function ListGroup()
   const items =['one ','two','three','four','five','six','seven']
     return (<ul className="list-group">
             <h1>List Name</h1>
-    {items.map(item => <li>{item} </li>)}
+    {items.map(item => <li key={item}> {item} </li>)}
   </ul>);
 }
 export default ListGroup;
