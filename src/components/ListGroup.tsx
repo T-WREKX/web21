6b590829ae359781155714cace41c5d7b0845cac
T-WREKX <abdulhosaint@gmail.com>
2025-02-11 12:28:41 +0530

    proto 8

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,6 +2,7 @@ function ListGroup()
 {
   const items =['one ','two','three','four','five','six','seven']
     return (<ul className="list-group">
+            <h1>List Name</h1>
     {items.map(item => <li>{item} </li>)}
   </ul>);
 }
