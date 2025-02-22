06af8bc4a016173a7c42cb34a4c5b7b384c93276
T-WREKX <abdulhosaint@gmail.com>
2025-02-22 15:12:28 +0530

    22

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,7 +4,7 @@ let items =['one ','two','three','four','five','six','seven']
     return (<ul className="list-group">
             <h1>List Name</h1>
             {items.length === 0 && <p>No item found</p>}
-            {items.map((item, index) => <li className="l-i-g" key={item} onClick={()=>console.log(event)}> {item} </li>)}
+            {items.map((item, index) => <li className="l-i-g" key={item} onClick={(event)=>console.log(event)}> {item} </li>)}
             </ul>);
 }
 export default ListGroup;
