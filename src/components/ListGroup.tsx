e2c844792f14660b576c69b6d7db2666ee4a0bae
T-WREKX <abdulhosaint@gmail.com>
2025-02-25 15:40:40 +0530

    23

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,12 @@
+import { MouseEvent } from "react";
 function ListGroup()
 {
+    const hClick=(event: MouseEvent)=>console.log(event);
 let items =['one ','two','three','four','five','six','seven']
     return (<ul className="list-group">
             <h1>List Name</h1>
             {items.length === 0 && <p>No item found</p>}
-            {items.map((item, index) => <li className="l-i-g" key={item} onClick={(event)=>console.log(event)}> {item} </li>)}
+            {items.map((item) => <li className="l-i-g" key={item} onClick={hClick}> {item} </li>)}
             </ul>);
 }
 export default ListGroup;
